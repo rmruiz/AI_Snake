70148fa59e9bdc591c8f5015ee6070e11bcba37b
Rolando Ruiz <rolando.ruiz@gmail.com>
2022-04-24 14:17:07 -0400

    new nn lib

---
+++ nn.py
@@ -0,0 +1,47 @@
+import numpy as np
+#import random
+#import pickle
+#import gzip
+
+#https://gist.github.com/roycoding/7bfcd821ae5be40804979973be149953
+
+def sigmoid(z):
+    """The sigmoid function."""
+    return 1.0/(1.0+np.exp(-z))
+
+def sigmoid_prime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
+
+class Network(object):
+
+    def __init__(self, sizes):
+        """The list ``sizes`` contains the number of neurons in the
+        respective layers of the network.  For example, if the list
+        was [2, 3, 1] then it would be a three-layer network, with the
+        first layer containing 2 neurons, the second layer 3 neurons,
+        and the third layer 1 neuron.  The biases and weights for the
+        network are initialized randomly, using a Gaussian
+        distribution with mean 0, and variance 1.  Note that the first
+        layer is assumed to be an input layer, and by convention we
+        won't set any biases for those neurons, since biases are only
+        ever used in computing the outputs from later layers."""
+        
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
+
+    def feedforward(self, a):
+        """Return the output of the network if ``a`` is input."""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+            print(a)
+        return a
+
+if __name__ == '__main__':
+    my_network = Network([4,3,6,1])
+
+    result = my_network.feedforward([0.3, 0.4, -0.2, -1])
+
+    print(result)
